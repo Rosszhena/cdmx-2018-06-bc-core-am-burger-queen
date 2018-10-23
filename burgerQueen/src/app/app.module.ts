6184717e30d1d496076c47bbea30570512d77120
Maria Rosa Rosete <ross.zhena@gmail.com>
2018-10-23 17:50:14 -0500

    initial heroku deploy'

--- burgerQueen/src/app/app.module.ts
+++ burgerQueen/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,12 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { PrivadoComponent } from './components/privado/privado.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { environment } from '../environments/environment';
+import { AuthService } from './servicios/auth.service';
 
 @NgModule({
   declarations: [
@@ -22,9 +29,12 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- burgerQueen/src/app/components/register/register.component.ts
+++ burgerQueen/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../servicios/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  public email: string;
+  public password: string;
 
-  constructor() { }
+  constructor(
+    public authService: AuthService,
+    public router: Router
+  ) { }
 
   ngOnInit() {
+  }
+  onSubmitAddUser() {
+    this.authService.registerUser(this.email, this.password)
+    .then((res) => {
+      this.router.navigate(['/privado']);
+    }).catch((err) => {
+      console.log(err);
+    });
   }
 
 }
